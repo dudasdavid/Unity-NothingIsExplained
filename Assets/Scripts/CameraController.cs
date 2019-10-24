dd069b29cba4fc9123a4f51c6ad4ce6201dc7ebc
Balázs Maczák <maczak.balazs@gmail.com>
2019-10-24 22:18:18 +0200

    Pixel perfect camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,28 +4,9 @@ using System.Collections;
 public class CameraController : MonoBehaviour
 {
 
-    public GameObject Player;        //Public variable to store a reference to the player game object
-    public float yOffset;
+    public Transform player;
 
-    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
-    private Vector3 userVector;
-    private float userXPrev;
-    private float originalUserVectorY;
-
-    private Vector2 screenSize;
-    private Vector3 cameraPos;
-
-    private Transform topCollider;
-    private Transform bottomCollider;
-    private Transform leftCollider;
-    private Transform rightCollider;
-
-    public float colDepth = 4f;
-    public float zPosition = 0f;
-
-    public float bottomOffset;
-    public float topOffset;
-
+    private Vector3 offset;        
     public bool followCameraY;
 
 
@@ -33,91 +14,15 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - Player.transform.position;
-        //offset.y += yOffset;
-        userVector = Player.transform.position;
-        userXPrev = userVector.x;
-        originalUserVectorY = userVector.y;
-
-
-        //Generate our empty objects
-        topCollider = new GameObject().transform;
-        bottomCollider = new GameObject().transform;
-        rightCollider = new GameObject().transform;
-        leftCollider = new GameObject().transform;
-
-        //Name our objects 
-        topCollider.name = "TopCollider";
-        bottomCollider.name = "BottomCollider";
-        rightCollider.name = "RightCollider";
-        leftCollider.name = "LeftCollider";
-
-        //Add the colliders
-        topCollider.gameObject.AddComponent<BoxCollider2D>();
-        bottomCollider.gameObject.AddComponent<BoxCollider2D>();
-        rightCollider.gameObject.AddComponent<BoxCollider2D>();
-        leftCollider.gameObject.AddComponent<BoxCollider2D>();
-
-        //Make them the child of whatever object this script is on, preferably on the Camera so the objects move with the camera without extra scripting
-        //topCollider.parent = transform;
-        bottomCollider.parent = transform;
-        rightCollider.parent = transform;
-        leftCollider.parent = transform;
-
-        //Generate world space point information for position and scale calculations
-        cameraPos = Camera.main.transform.position;
-        screenSize.x = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
-        screenSize.y = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
-
-        //Change our scale and positions to match the edges of the screen...   
-        rightCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
-        rightCollider.position = new Vector3(cameraPos.x + screenSize.x + (rightCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
-        leftCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
-        leftCollider.position = new Vector3(cameraPos.x - screenSize.x - (leftCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
-        topCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
-        topCollider.position = new Vector3(cameraPos.x, cameraPos.y + screenSize.y + (topCollider.localScale.y * 0.5f) + topOffset, zPosition);
-        bottomCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
-        bottomCollider.position = new Vector3(cameraPos.x, cameraPos.y - screenSize.y - (bottomCollider.localScale.y * 0.5f) - bottomOffset, zPosition);
-    }
+        offset = transform.position - player.transform.position;
+     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        //transform.position = Player.transform.position + offset;
-        userVector.x = Player.transform.position.x;
-
-        if (followCameraY)
-        {
-            userVector.y = Player.transform.position.y+3;
-        }
-        else
-        {
-            userVector.y = originalUserVectorY-yOffset;
-        }
-
-        if (userVector.x > userXPrev || followCameraY)
-        {
-            transform.position = userVector + offset;
-            userXPrev = userVector.x;
-        }
-        
+        Vector3 position = transform.position;
+        position.x = Mathf.Max(position.x, player.position.x);
+        transform.position = position;
     }
-
-    void FixedUpdate()
-    {
-        //Generate world space point information for position and scale calculations
-        cameraPos = Camera.main.transform.position;
-        screenSize.x = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
-        screenSize.y = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
-
-        //Change our scale and positions to match the edges of the screen...   
-        leftCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
-        leftCollider.position = new Vector3(cameraPos.x - screenSize.x - (leftCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
-
-        Debug.Log(topCollider.position);
-
-    }
-
 
 }
---
+++ Assets/Scripts/UiFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiFollow : MonoBehaviour
+{
+    public RectTransform uiElementToFollow;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = uiElementToFollow.position;
+    }
+}
